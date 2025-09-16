36a7ec64445a2cc4a68d4e33d2dd7e894794b0a5
Jagruthi3012 <jkelaval@buffalo.edu>
2025-09-16 15:22:40 -0400

    Updated contact

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,5 +1,5 @@
-import { FaDiscord, FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+
 
 const ContactData = {
   phone: "+1 716-520-8780",
