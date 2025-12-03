ee1d3d4b9556c37c06d77c0ab275239e6ada9b5e
Jagruthi3012 <jkelaval@buffalo.edu>
2025-12-03 11:09:43 -0500

    few changes

--- src/data/profile.js
+++ src/data/profile.js
@@ -3,7 +3,7 @@ const ProfileData = {
   name: "Jagruthi Kelavalli Jagadish",
   professions: ["Web Developer", "Software Engineer", "Cybersecurity Enthusiast"],
   info: [
-    "Aspiring Software Engineer pursuing an MS in Computer Science at the University at Buffalo,",
+    "Aspiring Software Engineer pursuing an M in Computer Science at the University at Buffalo,",
     "with hands-on experience in backend APIs, data visualizations, and network security."
   ],
   resume:
